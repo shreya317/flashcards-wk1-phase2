8d62a146071f010088bda7c3a4f3a6a8362f713b
Shreya Patel <shreya317@gmail.com>
2014-11-03 07:51:00 -0600

    updated turns controller

--- app/controllers/turns.rb
+++ app/controllers/turns.rb
@@ -11,47 +11,24 @@ post '/turns' do
   if user.turns.nil?
     @deck = Deck.find(params[:deck_id])
     current_cards = @deck.cards.shuffle
-    @turn = Turn.create!(player_id: session[:user_id], card: current_cards.shift)
+    @turn = Turn.create!(player_id: session[:user_id], card: current_cards.sample)
   else
-    @deck = Deck.find(params[:deck_id])
-    current_cards = @deck.cards.shuffle
-  @turn = Turn.create!(player_id: session[:user_id], card: current_cards.shift)
+    @deck = Turn.find(params[:turn_id]).card.deck
+    current_card = @deck.cards.sample
+    @turn = Turn.create!(player_id: session[:user_id], card: current_card)
   end
+      if @turn.card.correct?(params[:guess])
+        session[:message] = "Correct!"
+      else
+        session[:message] = "Sorry, that's incorrect!"
+      end
   redirect "/turns/#{@turn.id}"
 end
-
-#   if session[:turns_to_complete].nil?
 
 
 
 get '/turns/:turn_id' do
   @turn = Turn.find(params[:turn_id])
   @deck = Deck.find(@turn.card.deck.id)
-  if @last_turn = Turn.find(params[:turn_id].to_i - 1)
-    if @last_turn.card.correct?(params[:guess])
-      session[:message] = "Correct!"
-    else
-      session[:message] = "Sorry, that's incorrect!"
-    end
-  end
   erb :'/turns/show'
 end
-
-# post '/turns' do
-#   @current_card = Card.new
-#   if @current_card.answer == params[:user_guess]
-#   # if @current_card.check_guess(params[:user_guess])
-
-
-#     # session[:correct_message] = "Correct!"
-#     redirect :"/turns/:turn_id"
-#   else
-#     # puts "Incorrect"
-#     # session[:incorrect_meesage] = "Sorry, that's incorrect!"
-#     # redirect :'/turns/new'
-#   end
-# # take input from user
-# # check guess
-# # add card to played_cards
-# # redirect to turns/new
-# end
